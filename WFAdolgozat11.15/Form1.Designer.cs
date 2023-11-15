bada0405a9bc12974e70e9ed74786b02afccde3c
Farkas Ivett <farkasivettbianka16@gmail.com>
2023-11-15 13:16:12 +0100

    rtzui

--- WFAdolgozat11.15/Form1.Designer.cs
+++ WFAdolgozat11.15/Form1.Designer.cs
@@ -29,42 +29,213 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            button1 = new Button();
-            textBox1 = new TextBox();
+            KoszonjButton = new Button();
+            nevTextBox = new TextBox();
+            pirosLabel = new Label();
+            zoldLabel = new Label();
+            kekLabel = new Label();
+            UdvozlesLabel = new Label();
+            button2 = new Button();
+            alaphelyzetButton = new Button();
+            kilepesButton = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            button5 = new Button();
+            label7 = new Label();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(27, 35);
+            label1.Location = new Point(27, 31);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(44, 23);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "név:";
             // 
-            // button1
+            // KoszonjButton
             // 
-            button1.Location = new Point(202, 31);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            KoszonjButton.Location = new Point(254, 30);
+            KoszonjButton.Name = "KoszonjButton";
+            KoszonjButton.Size = new Size(95, 25);
+            KoszonjButton.TabIndex = 1;
+            KoszonjButton.Text = "Köszönj!";
+            KoszonjButton.UseVisualStyleBackColor = true;
+            KoszonjButton.Click += KoszonjButton_Click;
             // 
-            // textBox1
+            // nevTextBox
             // 
-            textBox1.Location = new Point(81, 31);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
+            nevTextBox.Location = new Point(77, 31);
+            nevTextBox.Name = "nevTextBox";
+            nevTextBox.Size = new Size(171, 23);
+            nevTextBox.TabIndex = 2;
+            // 
+            // pirosLabel
+            // 
+            pirosLabel.BackColor = Color.FromArgb(192, 0, 0);
+            pirosLabel.Location = new Point(414, 30);
+            pirosLabel.Name = "pirosLabel";
+            pirosLabel.Size = new Size(68, 63);
+            pirosLabel.TabIndex = 3;
+            pirosLabel.Text = "Piros";
+            // 
+            // zoldLabel
+            // 
+            zoldLabel.BackColor = Color.FromArgb(0, 192, 0);
+            zoldLabel.Location = new Point(488, 30);
+            zoldLabel.Name = "zoldLabel";
+            zoldLabel.Size = new Size(68, 63);
+            zoldLabel.TabIndex = 4;
+            zoldLabel.Text = "Zöld";
+            // 
+            // kekLabel
+            // 
+            kekLabel.BackColor = Color.Blue;
+            kekLabel.Location = new Point(562, 30);
+            kekLabel.Name = "kekLabel";
+            kekLabel.Size = new Size(68, 63);
+            kekLabel.TabIndex = 5;
+            kekLabel.Text = "Kék";
+            // 
+            // UdvozlesLabel
+            // 
+            UdvozlesLabel.BackColor = Color.FromArgb(255, 255, 128);
+            UdvozlesLabel.Font = new Font("Engravers MT", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            UdvozlesLabel.Location = new Point(27, 76);
+            UdvozlesLabel.Name = "UdvozlesLabel";
+            UdvozlesLabel.Size = new Size(322, 50);
+            UdvozlesLabel.TabIndex = 6;
+            UdvozlesLabel.Text = "###Udvozles###";
+            UdvozlesLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(27, 215);
+            button2.Name = "button2";
+            button2.Size = new Size(603, 40);
+            button2.TabIndex = 7;
+            button2.Text = "Számolj!";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // alaphelyzetButton
+            // 
+            alaphelyzetButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            alaphelyzetButton.ForeColor = Color.SteelBlue;
+            alaphelyzetButton.Location = new Point(254, 261);
+            alaphelyzetButton.Name = "alaphelyzetButton";
+            alaphelyzetButton.Size = new Size(180, 40);
+            alaphelyzetButton.TabIndex = 8;
+            alaphelyzetButton.Text = "Alaphelyzet";
+            alaphelyzetButton.UseVisualStyleBackColor = true;
+            alaphelyzetButton.Click += alaphelyzetButton_Click;
+            // 
+            // kilepesButton
+            // 
+            kilepesButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            kilepesButton.ForeColor = Color.Red;
+            kilepesButton.Location = new Point(450, 261);
+            kilepesButton.Name = "kilepesButton";
+            kilepesButton.Size = new Size(180, 40);
+            kilepesButton.TabIndex = 9;
+            kilepesButton.Text = "Kilépés";
+            kilepesButton.UseVisualStyleBackColor = true;
+            kilepesButton.Click += kilepesButton_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(27, 147);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(77, 51);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(272, 147);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(77, 51);
+            textBox3.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(364, 159);
+            label6.Name = "label6";
+            label6.Size = new Size(30, 30);
+            label6.TabIndex = 12;
+            label6.Text = "=";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(126, 142);
+            button5.Name = "button5";
+            button5.Size = new Size(30, 30);
+            button5.TabIndex = 13;
+            button5.Text = "+";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.Location = new Point(414, 159);
+            label7.Name = "label7";
+            label7.Size = new Size(190, 38);
+            label7.TabIndex = 14;
+            label7.Text = "label7";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(162, 142);
+            button6.Name = "button6";
+            button6.Size = new Size(30, 30);
+            button6.TabIndex = 15;
+            button6.Text = "-";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(126, 178);
+            button7.Name = "button7";
+            button7.Size = new Size(30, 30);
+            button7.TabIndex = 16;
+            button7.Text = "*";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(162, 178);
+            button8.Name = "button8";
+            button8.Size = new Size(30, 30);
+            button8.TabIndex = 17;
+            button8.Text = "/";
+            button8.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(label7);
+            Controls.Add(button5);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(kilepesButton);
+            Controls.Add(alaphelyzetButton);
+            Controls.Add(button2);
+            Controls.Add(UdvozlesLabel);
+            Controls.Add(kekLabel);
+            Controls.Add(zoldLabel);
+            Controls.Add(pirosLabel);
+            Controls.Add(nevTextBox);
+            Controls.Add(KoszonjButton);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -75,7 +246,22 @@
         #endregion
 
         private Label label1;
-        private Button button1;
-        private TextBox textBox1;
+        private Button KoszonjButton;
+        private TextBox nevTextBox;
+        private Label pirosLabel;
+        private Label zoldLabel;
+        private Label kekLabel;
+        private Label UdvozlesLabel;
+        private Button button2;
+        private Button alaphelyzetButton;
+        private Button kilepesButton;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label6;
+        private Button button5;
+        private Label label7;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
--- WFAdolgozat11.15/Form1.cs
+++ WFAdolgozat11.15/Form1.cs
@@ -5,6 +5,45 @@ namespace WFAdolgozat11._15_
         public Form1()
         {
             InitializeComponent();
+
+            pirosLabel.Click += SzinLabelClick;
+            zoldLabel.Click += SzinLabelClick;
+            kekLabel.Click += SzinLabelClick;
+
+            alaphelyzetButton.Click += alaphelyzetButton_Click;
+            kilepesButton.Click += kilepesButton_Click;
+        }
+
+
+        private void KoszonjButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(nevTextBox.Text))
+            {
+                string koszones = $"Szia, {nevTextBox.Text}!";
+                UdvozlesLabel.Text = koszones;
+            }
+            else
+            {
+                MessageBox.Show("Kérlek, add meg a nevedet!");
+            }
+        }
+        private void SzinLabelClick(object sender, EventArgs e)
+        {
+            if (sender is Label clickedLabel)
+            {
+                this.BackColor = clickedLabel.BackColor;
+            }
+        }
+
+        private void alaphelyzetButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            Environment.Exit(0);
+        }
+
+        private void kilepesButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
